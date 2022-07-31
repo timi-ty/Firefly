731fd85f3de706ebe32ae8f35c1195df36222932
Timi Tayo <timilehin.ty@gmail.com>
2022-07-31 21:31:48 +0100

    Initial Commit

---
+++ Assets/MathUtils.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MathUtils
+{
+    /// <summary>Returns the shortest angle in degrees from 0 to 180 between two floats.</summary>
+    public static float Angle(float from, float to)
+    {
+        float angle = Mathf.Abs(to - from) % 360;
+        return angle > 180 ? 360 - angle : angle;
+    }
+
+    /// <summary>Returns the shortest signed angle in degrees from 180 to -180 between two floats.</summary>
+    public static float AngleSigned(float from, float to)
+    {
+        float angle = (to - from) % 360;
+        if (angle > 180) return angle - 360;
+        if (angle < -180) return angle + 360;
+        return angle;
+    }
+		
+    /// <summary>Remaps any angle to the range -180 to 180.</summary>
+    public static float UnwarpAngle(float angle)
+    {
+        angle %= 360;
+        if (angle > 180) return angle - 360;
+        if (angle < -180) return angle + 360;
+        return angle;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Movement Settings
+
+    [SerializeField, Header("Movement Settings")]
+    private float _rotationAgility = 1.0f;
+    [SerializeField]
+    private float _directionAgility = 1.0f;
+    [SerializeField]
+    private float _maxSpeed = 1.0f;
+    [SerializeField]
+    private float _maxAcceleration = 1.0f;
+    [SerializeField]
+    private float _maxDeceleration = 1.0f;
+    [SerializeField, Tooltip("Normalized Curve")] 
+    private AnimationCurve _accelerationVsSpeed;
+    [SerializeField, Tooltip("Normalized Curve")] 
+    private AnimationCurve _decelerationVsSpeed;
+
+    #endregion
+
+    #region Components
+
+    private Rigidbody Rigidbody { get; set; }
+
+    #endregion
+
+    #region Worker Parameters
+
+    private float _currentRotation;
+    private float _targetRotation;
+    
+    private Vector2 _velocity;
+    private Vector2 _currentDirection;
+    private Vector2 _targetDirection;
+    private float _currentSpeed;
+    private float _targetSpeed;
+
+    #endregion
+
+    #region Unity Runtime
+
+    private void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _currentRotation = Mathf.MoveTowardsAngle(_currentRotation, _targetRotation,
+            MathUtils.Angle(_currentRotation, _targetRotation) * _rotationAgility * Time.deltaTime);
+
+        float acceleration;
+        float normalizedSpeed = _currentSpeed / _maxSpeed;
+        if (_targetSpeed > _currentSpeed)
+        {
+            acceleration = _accelerationVsSpeed.Evaluate(normalizedSpeed) * _maxAcceleration;
+        }
+        else
+        {
+            acceleration = _decelerationVsSpeed.Evaluate(normalizedSpeed) * _maxDeceleration;
+        }
+
+        float deltaSpeed = acceleration * Time.deltaTime;
+        _currentSpeed = Mathf.MoveTowards(_currentSpeed, _targetSpeed, deltaSpeed);
+        _currentDirection = Vector2.MoveTowards(_currentDirection, _targetDirection,
+            Vector2.Distance(_currentDirection, _targetDirection) * _directionAgility * Time.deltaTime);
+            
+        _velocity = _currentDirection * _currentSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody.rotation = Quaternion.Euler(0, _currentRotation, 0);
+
+        Vector2 nextPosition = _velocity * Time.fixedDeltaTime;
+        Rigidbody.position += new Vector3(nextPosition.x, 0, nextPosition.y);
+    }
+
+    #endregion
+
+    #region Input Receivers
+
+    public void MoveInput(InputAction.CallbackContext context)
+    {
+        var moveInput = context.ReadValue<Vector2>();
+
+        _targetDirection = moveInput.normalized;
+        _targetSpeed = moveInput.magnitude * _maxSpeed;
+    }
+
+    public void LookInput(InputAction.CallbackContext context)
+    {
+        var lookInput = context.ReadValue<Vector2>();
+
+        if (lookInput.sqrMagnitude < 0.1f) return;
+
+        _targetRotation = Vector2.SignedAngle(lookInput, Vector2.up);
+    }
+
+    #endregion
+}
